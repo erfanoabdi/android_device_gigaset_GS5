f174bea011cf82be96db3591fab15f7ffa22a81c
Erfan Abdi <erfangplus@gmail.com>
2021-12-08 16:18:00 +0330

    GS5: Build update engine packages

--- device.mk
+++ device.mk
@@ -92,3 +92,12 @@ PRODUCT_SOONG_NAMESPACES += \
 # Trust HAL
 PRODUCT_PACKAGES += \
     lineage.trust@1.0-service
+
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
