3de849ca674c895916647e9c8a6b59fe8412deb8
bengris32 <bengris32@protonmail.ch>
2022-07-20 23:37:58 +0530

    GS5: Introduce Mediatek In-Call Service
    * My attempt at fixing in call gain control on MediaTek devices.
    
    * It attempts to replicate what the stock MediaTek framework does
      when controlling the volume of the earpiece speaker, since MTK's
      audio HAL is not able to set the gain of the speaker from the
      values sent by AOSP framework.
    
    [LinkBoi00] Rebrand to Mediatek In-Call Service
    
    Change-Id: I43f727f6fb73a0d38457a41a8361653d1d00e220
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- device.mk
+++ device.mk
@@ -61,6 +61,9 @@ PRODUCT_PACKAGES += \
     libdynproc \
     libhapticgenerator
 
+PRODUCT_PACKAGES += \
+    MtkInCallService
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
