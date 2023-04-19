b06d58c8d7b6bde7b4ab89eecc6b2ead1f2eac66
Erfan Abdi <erfangplus@gmail.com>
2023-04-19 17:56:56 +0330

    nashc: Build required media libs * Mainly dependencies of C2 HAL.
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Icc8ecc63094a4d1fb9d669f9da93184b285fd471

--- device.mk
+++ device.mk
@@ -93,6 +93,15 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# Media
+PRODUCT_PACKAGES += \
+    libcodec2_hidl@1.1.vendor \
+    libcodec2_hidl@1.2.vendor \
+    libavservices_minijail_vendor \
+    libstagefright_softomx_plugin.vendor \
+    libsfplugin_ccodec_utils.vendor \
+    libcodec2_soft_common.vendor
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-mediatek
