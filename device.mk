0a49a6b996df141ebb389944465535c9ca58c140
nift4 <nift4@protonmail.com>
2023-04-21 00:13:01 +0330

    GS5: Build hal service packages in match with stock

--- device.mk
+++ device.mk
@@ -78,10 +78,15 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Biometrics
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth@1.1.vendor
+    android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
@@ -116,7 +121,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0.vendor \
     android.hardware.graphics.allocator@3.0.vendor \
     android.hardware.graphics.allocator@4.0.vendor \
-    android.hardware.graphics.composer@2.2-service \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-service \
     android.hardware.memtrack@1.0-impl \
     libdrm.vendor \
@@ -176,8 +181,8 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0.vendor \
-    libkeymaster4.vendor:64 \
-    libkeymaster4support.vendor:64 \
+    android.hardware.keymaster@4.1.vendor \
+    libkeymaster41.vendor:64 \
     libsoft_attestation_cert.vendor:64 \
     libkeystore-wifi-hidl \
     libkeystore-engine-wifi-hidl
