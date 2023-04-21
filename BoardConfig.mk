8b98c10d3e8c79dabefc2e3bc11f6bb42fa7ecf3
nift4 <nift4@protonmail.com>
2023-04-21 22:10:18 +0330

    GS5: Initial SEPolicy setup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,6 +128,7 @@ VENDOR_SECURITY_PATCH := 2023-03-05
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
