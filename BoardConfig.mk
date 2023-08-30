cece6b7904116c5254b3be888a18f4d451c630e0
techyminati <sinha.aryan03@gmail.com>
2023-08-30 18:56:44 +0530

    GS5: Update Blobs from GS5_12_V09
    * From: GS5-user-12 SP1A.210812.016 1685498763 release-keys
    
    Change-Id: Ie09dbe00695c9c83dd60d716f1a75a8d875d1c78
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,7 +127,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # SPL
-VENDOR_SECURITY_PATCH := 2023-03-05
+VENDOR_SECURITY_PATCH := 2023-05-05
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
